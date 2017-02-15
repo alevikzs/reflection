f8723326ecc617df4cccaa5845f4117a2c2a61ef
alexey novikov <alekseeey@gmail.com>
2017-02-16 00:22:08 +0200

    added main structure

---
+++ src/ReflectionClassUses.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace UsesReflection;
+
+use \ReflectionClass;
+
+/**
+ * Class ReflectionClassUses
+ * @package UsesReflection
+ */
+class ReflectionClassUses extends ReflectionClass {
+
+}
---
+++ tests/ReflectionClassUsesTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Tests;
+
+use \PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * Class ClassParserTest
+ * @package Tests
+ */
+class ClassParserTest extends TestCase {
+
+    public function testMain() {
+
+    }
+
+}
